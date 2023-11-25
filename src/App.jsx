90a94d5ef533a56cdda7608ffd715de0b5f03a96
iamsleepy <yuva@ryuki.me>
2023-11-25 22:17:07 +0530

    First working without changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,103 @@
 import './App.css';
-import {TextField, Button} from "@mui/material";
-import {useState} from "react";
+import {TextField, IconButton, Grid, ImageList, ImageListItem, CircularProgress} from "@mui/material";
+import DoneIcon from '@mui/icons-material/Done';
+import {useEffect, useState} from "react";
+import query from "./api";
 
 
-
-function FormComic() {
-    const [text, setText] = useState("");
-
+function FormComic({updateText, formIndex}) {
     function handleSubmit(event) {
         // hasSubmitted = true;
-        setText(event.target.value);
+        event.preventDefault();
+        const text = event.target.elements.text.value;
+        // console.log(text);
+        updateText(text, formIndex);
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField/>
-            <Button type="submit">Hello</Button>
-        </form>
+        <div className="text-box">
+            <form onSubmit={handleSubmit}>
+                <TextField id="text"/>
+                <IconButton type="submit">
+                    <DoneIcon/>
+                </IconButton>
+            </form>
+        </div>
+
     );
 }
+function FormImage({imgBlob, getImage, imageIndex}) {
 
-function afterSubmit(){
+    return (
+        <ImageListItem key={imageIndex} className="image-list-item">
+            <img src={imgBlob} loading="lazy" alt="generated using prompt"/>
+        </ImageListItem>
+    )
+}
+
+function Slider(){
 
 }
 
 function App() {
+    const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [textArr, setTextArr] = useState(Array(10).fill(""))
+    let [imageArr, setImageArr] = useState({})
+
+    const updateText = async (text, index) => {
+        // console.log(text);
+        // console.log(index);
+        setTextArr(prevState => {
+            let newState = [...prevState];
+            newState[index] = text;
+            return newState;
+        });
+        await updateImage(index, text);
+    }
+
+    const updateImage = async (imagePos, text) => {
+        setImageArr(prevState => {
+            let newState = {...prevState};
+            newState[imagePos] = "public/giphy.webp";
+            return newState;
+        });
+        const response = await query({"inputs": text});
+        const imageUrl = URL.createObjectURL(response);
+        setImageArr(prevState => {
+            let newState = {...prevState};
+            newState[imagePos] = imageUrl;
+            return newState;
+        });
+    }
+
+
+    const comicForms = ids.map((id) => {
+        return <FormComic updateText={updateText} key={id} formIndex={id}/>
+    })
+    const comicImages = Object.entries(imageArr).map(([key, value]) => {
+        return <FormImage imgBlob={value} key={key} imageIndex={key}/>
+    })
+
     return (
-    <div className="App">
-      <header className="App-header">
-        <FormComic/>
-      </header>
-    </div>
+        <Grid container spacing={2}>
+            <Grid item xs={2}>
+                {comicForms}
+            </Grid>
+            <Grid item>
+                <ImageList>
+                    {comicImages}
+                </ImageList>
+            </Grid>
+        </Grid>
   );
 }
 
 export default App;
+// <div className="app-body">
+//     <div className="forms-list">
+//         {comicForms}
+//     </div>
+//     <div className="image-list">
+//         {comicImages}
+//     </div>
+// </div>
